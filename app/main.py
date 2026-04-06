d9018cc4a0d3da231c4221d7f416a7d8a7e0b221
Bogdan <dishluk.b@gmail.com>
2026-04-06 11:52:48 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if not command:
+        return
+
+    command_list: str = command.split()
+
+    if len(command_list) != 3:
+        return
+
+    if command_list[0] != "cp":
+        return
+
+    source_file: str = command_list[1]
+    destination_file: str = command_list[2]
+
+    if source_file == destination_file:
+        return
+
+    try:
+        with open(source_file, "r") as file_in, open(destination_file, "w") as file_out:
+            content = file_in.read()
+            file_out.write(content)
+
+    except FileNotFoundError:
+        return
